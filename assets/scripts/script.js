859e1b47367ae297b1fd9a8854ac33a7ef4d2728
bananaWeb <a89.volkov@gmail.com>
2018-03-12 18:49:42 +0300

    I LOVE CAKES

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -6,9 +6,12 @@ $(document).ready(function() {
 	];
 	$('#particles-js').css({
 		'background-image':'url(assets/images/' + backgrounds[Math.floor(Math.random() *(3-0) + 0)] + ')'
- 	})
-		$('.nav-toggler').click(function(){
-		$('.mobile-nav').toggleClass('-is-open');
+ 	})		
+		$('.nav-toggler').click(function() {
+       if (modal.hasClass('-active'))    {
+            showHideModal();
+         }
+		$('.mobile-nav').toggleClass('-open');
 		$('.block1').toggleClass('-invisible');
 		$('.block2').toggleClass('-turn');
 		$('.block3').toggleClass('-invisible');
@@ -17,4 +20,33 @@ $(document).ready(function() {
 			$('.section-main h1').addClass('-animated');
 			$('.section-main h2').addClass('-animated');
 		}, 1000);
+
+let modalToggler = $('.js-modal-toggler'),
+      modal = $('.modal'),
+      mobileNav = $('.mobile-nav'),
+      modalElems = [
+         '.js-icon-call',
+         '.js-icon-mail',
+         '.js-icon-vk',
+         '.js-icon-git',
+         '.modal-map',
+         '.modal-toggler'
+      ];
+
+   modalToggler.click(function ($event) {
+      if (mobileNav.hasClass('-open')) {
+         mobileNav.removeClass('-open');
+      }
+      event.preventDefault();
+      showHideModal();
+   })
+   function showHideModal(){
+      modal.toggleClass("-active");
+      for (let i = 0; i < modalElems.length; i++){
+         let delay =  i * 400;   
+         setTimeout(function(){
+            $(modalElems[i]).toggleClass('-active');
+         }, 400 + delay)
+      }
+   }
 });
